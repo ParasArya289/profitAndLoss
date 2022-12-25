a4f41fe07728e1292d5b36da1bdc99937ef614cf
ParasArya289 <parasarya289@gmail.com>
2022-12-26 00:32:25 +0530

    fix: improved validation

--- src/profitAndLoss.js
+++ src/profitAndLoss.js
@@ -8,29 +8,38 @@ let ProfitAndLoss = () => {
   let [clr, setClr] = useState("grey");
 
   let checkProfitAndLoss = () => {
-    if (initial && current && quantity) {
+    if (
+      initial &&
+      current &&
+      quantity &&
+      initial > 0 &&
+      current > 0 &&
+      quantity > 0
+    ) {
       if (initial > current) {
         let loss = (initial - current) / quantity;
         let lossPercentage = (loss / initial) * 100;
         setOutput(
-          `Your loss is ${loss.toFixed(2)}, loss rate is ${lossPercentage.toFixed(2)}%`
+          `Your loss is ${loss.toFixed(
+            2
+          )}, loss rate is ${lossPercentage.toFixed(2)}%`
         );
         setClr("#dc2626");
       } else if (current > initial) {
         let profit = (current - initial) / quantity;
         let profitPercentage = (profit / initial) * 100;
         setOutput(
-          `Your profit is ${profit.toFixed(2)}, growth rate is ${profitPercentage.toFixed(
+          `Your profit is ${profit.toFixed(
             2
-          )}%`
+          )}, growth rate is ${profitPercentage.toFixed(2)}%`
         );
         setClr("#22c55e");
       } else {
         setOutput("Be patient your profit will grow");
         setClr("grey");
       }
-    }else{
-        alert('Please fill all the Fields');
+    } else {
+      alert("Please fill all the Fields and make sure to enter positive values");
     }
   };
 
